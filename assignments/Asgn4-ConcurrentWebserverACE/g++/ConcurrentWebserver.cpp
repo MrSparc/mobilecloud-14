1d593a27de46f8f2054efaed421aa94aa428b331
MrSparc <mrsparc@users.noreply.github.com>
2014-09-27 23:50:15 +0100

    Echo server completed

--- assignments/Asgn4-ConcurrentWebserverACE/g++/ConcurrentWebserver.cpp
+++ assignments/Asgn4-ConcurrentWebserverACE/g++/ConcurrentWebserver.cpp
@@ -34,9 +34,8 @@
 #include "ace/OS_NS_Thread.h"
 
 
-
+// Number of threads
 #define POOL_SIZE 5
-#define MAX_TIMEOUT 5
 
 /* Stores a string version of the current thread id into buffer and
  * returns the size of this thread id in bytes.
@@ -44,98 +43,40 @@
 ssize_t ACE_OS_thr_id(char buffer[])
 {
 #if defined (ACE_WIN32)
-return ACE_OS::sprintf(buffer,
+  return ACE_OS::sprintf(buffer,
 			 "Thread id: <%u>",
-		       static_cast <unsigned> (ACE_Thread::self()));
+			 static_cast <unsigned> (ACE_Thread::self()));
 #else
-ACE_hthread_t t_id;
-ACE_OS::thr_self(t_id);
-return ACE_OS::sprintf(buffer,
+  ACE_hthread_t t_id;
+  ACE_OS::thr_self(t_id);
+  return ACE_OS::sprintf(buffer,
 			 "Thread id: <%lu>",
-		       (unsigned long)t_id);
+			 (unsigned long)t_id);
 #endif /* WIN32 */
 }
 
 
+// Forward declaration
+class Echo_Svc_Handler;
+
 /**
  * @class Echo_Task
- * @brief 
+ * @brief Echo task worker
  *
- * Create an Echo_Task that inherits from ACE_Task (configured with the ACE_MT_SYNCH traits class to obtain a synchronized request queue) 
+ * Create an Echo_Task that inherits from ACE_Task 
+ * (configured with the ACE_MT_SYNCH traits class to obtain a synchronized request queue)
  */
 class Echo_Task : public ACE_Task < ACE_MT_SYNCH >
 {
 public:
-  Echo_Task()
-  { }
+  Echo_Task();
+  void echo_svc_handler(Echo_Svc_Handler *);
+  virtual int svc(void);
 
-  virtual int svc(void)
-  {
-    ACE_TRACE(ACE_TEXT("Workers::svc"));
-
-    ACE_DEBUG((LM_INFO, ACE_TEXT("(%t) Workers started\n")));
-    while (1)
-      {
-	ACE_Message_Block *mb = NULL;
-
-	if (this->getq(mb) == -1)
-	  {
-	    ACE_DEBUG((LM_INFO,
-		       ACE_TEXT("(%t) Shutting down\n")));
-	    break;
-	  }
-
-
-        ACE_DEBUG((LM_INFO, ACE_TEXT("(%t) Call process_message\n")));
-
-	//First sends back to the client [ACE_SOCK_Stream] the thread id [ACE_OS::thr_self()] that is running the svc() method 
-	//char threadId[256];
-	//size_t threadIdSize = ACE_OS_thr_id(threadId);
-	// if ( this->send(threadId, threadIdSize) < 0)
-	//   {
-	//     ACE_ERROR_RETURN((LM_ERROR,
-	// 		      "%s\n",
-	// 		      "send_n failed"),
-	// 		     -1);
-	//   }
-
-	// Process the message.	
-	process_message(mb);
-      }
-
-    return 0;
-  }
 private:
-  void process_message(ACE_Message_Block *mb)
-  {
-    ACE_DEBUG((LM_INFO, ACE_TEXT("(%t) Workers::process_message\n")))
-    ACE_TRACE("Workers::process_message");
-    char buf[ACE_DEFAULT_MAX_SOCKET_BUFSIZ];
-		
+  void process_message(ACE_Message_Block *);
 
-
-    size_t length = mb->length();
-		
-    if (length > 0)
-      ACE_DEBUG((LM_DEBUG,
-		 "(%t) in iteration length = %d, text = \"%*s\"\n",
-		 length,
-		 length - 1,
-		 mb->rd_ptr()));
-    ACE_OS::memcpy(buf, mb->rd_ptr(), length);
-    mb->release();
-
-    ACE_DEBUG((LM_DEBUG,
-	       ACE_TEXT("(%t) Started processing message %s\n"),
-	       buf));
-
-    /// This sleep emulates a long operation 
-    ACE_OS::sleep(3); 
-
-    ACE_DEBUG((LM_DEBUG,
-	       ACE_TEXT("(%t) Finished processing message %s\n"),
-	       buf));
-  }
+  Echo_Svc_Handler *echo_svc_handler_;
 };
 
 
@@ -143,61 +84,19 @@ private:
  * @class Echo_Svc_Handler
  * @brief Service handler using TCP sockets stream (using a wrapper facade SOCK_STREAM)
  *
- * Create an Echo_Svc_Handler that inherits from ACE_Svc_Handler 
- * (configured with the ACE_SOCK_STREAM class and ACE_NULL_SYNCH traits class) 
- * and implement its handle_input() hook method so that it :
- * Reads the client data [ACE_SOCK_Stream] until the end of a line is reached 
- * (i.e., the symbols "\n", "\r", or "\r\n" are read). 
- * Puts the client data into a message [ACE_Message_Block], and 
- * Calls Echo_Task::put(), which uses ACE_Task::putq() to enqueue the message for 
- * subsequent processing by a thread in the pool of threads that are running the Echo_Task::svc() hook method.   
+ * Create an Echo_Svc_Handler that inherits from ACE_Svc_Handler
+ * (configured with the ACE_SOCK_STREAM class and ACE_NULL_SYNCH traits class)
  */
 class Echo_Svc_Handler : public ACE_Svc_Handler < ACE_SOCK_STREAM, ACE_NULL_SYNCH >
 {
 
 public:
+  void echo_task(Echo_Task *);
+  virtual int handle_input(ACE_HANDLE);
 
-  void echo_task(Echo_Task *et){
-    echo_task_ = et;
-  }; 
-
-protected:
-
-  virtual int handle_input(ACE_HANDLE)
-  {
-    ACE_TRACE ("Echo_Svc_Handler::handle_input");
-    ACE_DEBUG((LM_DEBUG,
-	       "(%t) Echo_Svc_Handler::handle_input\n"));
-	
-    /// TODO:  Reads the client data [ACE_SOCK_Stream] until the end of a line is reached 
-    /// (i.e., the symbols "\n", "\r", or "\r\n" are read). 
-    char buf[ACE_DEFAULT_MAX_SOCKET_BUFSIZ];
-    ssize_t recv_cnt;
-
-    recv_cnt = this->peer().recv(buf, sizeof(buf));
-    if (recv_cnt <=0) {
-      ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("(% P |% t) connection closed \n")));
-      return -1;
-    }
-
-    ACE_Message_Block *mb = 0;
-    ACE_NEW_RETURN(mb, 
-		   ACE_Message_Block(recv_cnt),
-		   -1);
-
-    ACE_OS::memcpy(mb->wr_ptr(), buf, recv_cnt);
-
-    
-    echo_task_->putq(mb);
-
-    return 0;
-  }
-	
 private:
-  Echo_Task * echo_task_;
+  Echo_Task *echo_task_;
 };
-
-
 
 
 /**
@@ -210,45 +109,165 @@ private:
  */
 class Echo_Acceptor : public  ACE_Acceptor <Echo_Svc_Handler, ACE_SOCK_ACCEPTOR > {
 public:
+  void echo_task(Echo_Task *);
+  virtual int make_svc_handler(Echo_Svc_Handler *&);
 
-  void echo_task(Echo_Task *et){
-    echo_task_ = et;
-  }
-  Echo_Task * echo_task(void){
-    return echo_task_;
-  }
-
-  /**
-   * Bridge method for creating a Echo_Svc_Handler.
-   * Is overrided to perform Echo_Svc_Handler creation and assign
-   * the Echo_Task
-   * linking the handler, etc.).  Returns -1 on failure, else 0.
-   */
-  virtual int make_svc_handler(Echo_Svc_Handler *&sh)
-  {
-    ACE_TRACE("Echo_Acceptor::make_svc_handler");
-
-    if (sh == 0)
-      ACE_NEW_RETURN(sh,
-		     Echo_Svc_Handler,
-		     -1);
-		
-    /// Assign to each new handler the same thread pool
-    sh->echo_task(this->echo_task_);
-		
-
-    // Set the reactor of the newly created <SVC_HANDLER> to the same
-    // reactor that this <ACE_Acceptor> is using.
-    sh->reactor(this->reactor());
-    return 0;
-  }
-
-private :
+private:
   Echo_Task *echo_task_;
-
 };
 
 
+
+
+Echo_Task::Echo_Task()
+{
+  ACE_DEBUG((LM_INFO,
+	     ACE_TEXT("(%t) Echo_task::Constructor\n")));
+}
+	
+void Echo_Task::echo_svc_handler( Echo_Svc_Handler *svc)
+{
+  echo_svc_handler_ = svc;
+}
+
+/// Implement its svc() hook method to perform the "half-sync"
+int Echo_Task::svc(void)
+{
+  ACE_DEBUG((LM_INFO,
+	     ACE_TEXT("(%t) Echo_task::svc\n")));
+
+  while (1)
+    {
+      // Dequeueing messages (ACE_Message_Blocks obtained via ACE_Task::getq()) 
+      // containing the client input that was put into its synchronized request queue
+      ACE_Message_Block *mb = NULL;
+      if (this->getq(mb) == -1)
+	{
+	  ACE_DEBUG((LM_INFO,
+		     ACE_TEXT("(thread_id:%t) Shutting down\n")));
+	  break;
+	}
+
+      ACE_DEBUG((LM_INFO, 
+		 ACE_TEXT("(thread_id:%t) Call process_message\n")));
+
+      // Process the message (sends back the thread_id and original message)
+      process_message(mb);
+    }
+
+  return 0;
+}
+
+void Echo_Task::process_message(ACE_Message_Block *mb)
+{
+  ACE_DEBUG((LM_INFO,
+	     ACE_TEXT("(thread_id:%t) Echo_Task::process_message\n")));
+
+  char buf[ACE_DEFAULT_MAX_SOCKET_BUFSIZ];
+
+  size_t length = mb->length();
+
+  ACE_DEBUG((LM_INFO,
+	     "(thread_id:%t) Message Length %d\n", length));
+
+  ACE_OS::memcpy(buf, mb->rd_ptr(), length);
+  mb->release();
+
+  buf[length] = 0;
+
+  ACE_DEBUG((LM_DEBUG,
+	     ACE_TEXT("(thread_id:%t) Started processing message %s\n"),
+	     buf));
+
+  // Sends back to client the thread-id
+  char tid[256];
+  ssize_t tid_length = ACE_OS_thr_id(tid);
+  echo_svc_handler_->peer().send(tid, tid_length);
+
+  // Sends back to client the original data
+  echo_svc_handler_->peer().send(buf, length);
+
+  ACE_OS::sleep(3); /// This sleep emulates a long operation
+
+  ACE_DEBUG((LM_DEBUG,
+	     ACE_TEXT("(thread_id:%t) Finished processing message %s\n"),
+	     buf));
+}
+	       
+
+
+void Echo_Svc_Handler::echo_task(Echo_Task *et){
+  echo_task_ = et;
+  echo_task_->echo_svc_handler(this);
+}
+
+
+
+//  implement its handle_input() hook method 
+int Echo_Svc_Handler::handle_input(ACE_HANDLE)
+{
+  ACE_DEBUG((LM_DEBUG,
+	     "(thread_id:%t) Echo_Svc_Handler::handle_input\n"));
+
+
+  // Reads the client data [ACE_SOCK_Stream] until the end of a line is reached
+  char buf[ACE_DEFAULT_MAX_SOCKET_BUFSIZ];
+  ssize_t recv_cnt;
+
+  recv_cnt = this->peer().recv(buf, sizeof(buf));
+  if (recv_cnt <= 0) {
+    ACE_DEBUG((LM_DEBUG, ACE_TEXT("(% P |% t) connection closed \n")));
+    return -1;
+  }
+
+  // Puts the client data into a message [ACE_Message_Block]
+  ACE_Message_Block *mb = 0;
+  ACE_NEW_RETURN(mb,
+		 ACE_Message_Block(recv_cnt),
+		 -1);
+
+  mb->copy(buf, recv_cnt);
+
+  // Calls Echo_Task::put(), which uses ACE_Task::putq() to enqueue the message 
+  // for subsequent processing by a thread in the pool of threads that are running
+  // the Echo_Task::svc() hook method.  
+  echo_task_->putq(mb);
+
+  return 0;
+}
+
+
+
+	
+// Echo_Task setter
+void Echo_Acceptor::echo_task(Echo_Task *et)
+{
+  echo_task_ = et;
+}
+	
+/**
+ * Bridge method used to create the new service handler object [Echo_Svc_Handler].
+ * Customized to perform Echo_Svc_Handler creation and set a pointer to the Echo_Task
+ * Returns -1 on failure, else 0.
+ */
+int Echo_Acceptor::make_svc_handler(Echo_Svc_Handler *&sh)
+{
+  ACE_DEBUG((LM_DEBUG,
+	     "(thread_id:%t) Echo_Acceptor::make_svc_handler\n"));
+
+  if (sh == 0)
+    ACE_NEW_RETURN(sh,
+		   Echo_Svc_Handler,
+		   -1);
+
+  /// Assign to each new handler the same thread pool
+  sh->echo_task(this->echo_task_);
+
+  // Set the reactor of the newly created <SVC_HANDLER> to the same
+  // reactor that this <ACE_Acceptor> is using.
+  sh->reactor(this->reactor());
+  return 0;
+}
 
 
 
@@ -259,23 +278,36 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
   u_short port = argc < 2 ? ACE_DEFAULT_SERVER_PORT : ACE_OS::atoi(argv[1]);
   ACE_INET_Addr addr(port);
 
-  ACE_Reactor *reactor = ACE_Reactor::instance();
-
-  Echo_Task * echo_task;
-  ACE_NEW_RETURN(echo_task,Echo_Task,-1);
-  echo_task->activate(THR_NEW_LWP | THR_JOINABLE, POOL_SIZE);
-
-  Echo_Acceptor acceptor;
-  acceptor.echo_task(echo_task);	
-  acceptor.open(addr, ACE_Reactor::instance());
+  ACE_DEBUG((LM_DEBUG,
+	     "(thread_id:%t) Program's entry point\n"));
 
   ACE_OS::printf("listening at port %d\n", port);
 
+  // Implement a main() function that:
+
+  //1. Creates an Echo_Task instance and have it spawn a pool of N threads
+  // (where N > 1) within itself (Echo_Task::activate()).
+  Echo_Task echo_task;
+  Echo_Task *ptask = &echo_task;
+  //Create POOL_SIZE kernel-level threads and allow the new threads to be joined with.
+  echo_task.activate(THR_NEW_LWP | THR_JOINABLE, POOL_SIZE);  
+
+  ///2. Creates an Echo_Acceptor instance and associate it with the Echo_Task.
+  Echo_Acceptor acceptor;
+  acceptor.echo_task(ptask); // Using a setter method defined in Echo_Acceptor
+
+  //3. Creates an  ACE_Reactor(or use the singleton instance of the ACE_Reactor)
+  ACE_Reactor *reactor = ACE_Reactor::instance();
+	
+  //4. Registers the Echo_Acceptor instance with the reactor
+  acceptor.open(addr, reactor);
+
+  //5. Run the reactor's event loop [ACE_Reactor::run_reactor_event_loop()] 
+  // to wait for connections/data to arrive from a client. 
   reactor->run_reactor_event_loop();
 
+  // Wait for all threads to exit
   ACE_Thread_Manager::instance()->wait();
 
-  delete echo_task;
-
-  return 0;	
+  return 0;
 }
